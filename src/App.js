1c0bf48d4dfc61460d3dc90f845b9813c99b4a66
Sananjayan Paramanantharajah <sananjayan.paramanantharajah@stud.kbw.ch>
2023-03-06 16:11:18 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [score, setScore] = useState(0)
+  
+
+  function threepointer (score){
+    var probrandom = Math.floor((Math.random() * 100) + 1)
+    if (probrandom > 80){
+      let tmp = counter + 3
+      setScore(tmp)
+    }
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Basketball
+      <button type="button" value="Threepointer" onClick={threepointer}/>
+      
     </div>
   );
 }
